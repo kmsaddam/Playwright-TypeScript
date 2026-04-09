f8d25fcdf9996f00f1f85390fa6af733ebc9fb3c
K M Saddam Hossain <dev.saddam@gmail.com>
2026-04-09 12:11:17 +0600

    Option Item Check

--- tests/select.spec.ts
+++ tests/select.spec.ts
@@ -1,7 +1,17 @@
-import test, { expect, page } from "@playwright/test";
+import { expect, Location, Locator, test } from "@playwright/test";
 
 test('Select Box', async({page}) => {
     await page.goto("https://testautomationpractice.blogspot.com/")
 
     await page.locator('//select[@id="country"]').selectOption({ value: 'brazil' });
+
+
+    let countriesSelect:Locator = page.locator('//select[@id="country"]/option');
+    let countries = (await countriesSelect.allInnerTexts()).map(t=>t.trim());
+
+    console.log(countries);
+
+   await expect(countries).toContain("Japan");
+
+    await page.waitForTimeout(5000)
 })
